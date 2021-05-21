5c82837f394a73fefae43adeea34418804c257db
hui的se <1196768261@qq.com>
2021-05-21 20:29:31 +0800

    init:上传github

--- src/webServer/index.js
+++ src/webServer/index.js
@@ -2,13 +2,16 @@ import express from "express";
 import expressws from "express-ws";
 import logger from "../common/logger.js";
 import hander from "./ws_spider.js"
+import path from "path"
+const relativeDir = path.dirname(import.meta.url).slice(8);
+const { join } = path;
 
 let app = express();
 expressws(app);
-app.use("/src", express.static("../src"));
+app.use("/", express.static(join(relativeDir, "../web/")));
 app.ws("/spider", hander)
 app.get("/", function (req, res) {
-    res.redirect('/src/index.html')
+    res.sendFile(join(relativeDir, "../web/index.html"));
 })
 app.listen(4000, () => {
     logger.mark(`服务器已开启，地址为: http://localhost:${4000}`)
